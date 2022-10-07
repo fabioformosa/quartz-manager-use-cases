46bf2337291e72595ac9cb0cc2c4c509c4b53460
fabio.formosa <fabio.formosa@gmail.com>
2022-10-07 20:23:01 +0200

    renamed a maven submodule

--- existing-security-cookie-based/src/main/java/it/fabioformosa/labs/springwithsecurity/ExistingSecurityCookieBased.java
+++ existing-security-cookie-based/src/main/java/it/fabioformosa/labs/springwithsecurity/ExistingSecurityCookieBased.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWithSecurityApplication {
+public class ExistingSecurityCookieBased {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWithSecurityApplication.class, args);
+        SpringApplication.run(ExistingSecurityCookieBased.class, args);
     }
 
 }
