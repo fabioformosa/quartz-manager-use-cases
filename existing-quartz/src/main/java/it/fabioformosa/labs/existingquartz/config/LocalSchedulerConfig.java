46126b615ee5b51332f3fb4650f8d93d917ddcf4
fabio.formosa <fabio.formosa@gmail.com>
2022-10-08 13:07:46 +0200

    added a new use case based on an existing quartz scheduler

---
+++ existing-quartz/src/main/java/it/fabioformosa/labs/existingquartz/config/LocalSchedulerConfig.java
@@ -0,0 +1,55 @@
+package it.fabioformosa.labs.existingquartz.config;
+
+import it.fabioformosa.quartzmanager.api.scheduler.AutowiringSpringBeanJobFactory;
+import org.quartz.spi.JobFactory;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class LocalSchedulerConfig {
+
+    @Bean
+    public JobFactory jobFactory(ApplicationContext applicationContext) {
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+
+    @ConditionalOnResource(resources = {"quartz.properties"})
+    @Bean
+    public Properties quartzProperties() throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+        propertiesFactoryBean.afterPropertiesSet();
+        return propertiesFactoryBean.getObject();
+    }
+
+    @Bean(name = "primaryScheduler")
+    public SchedulerFactoryBean localSchedulerFactoryBean( JobFactory jobFactory, Properties quartzProperties) {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setJobFactory(jobFactory);
+        factory.setQuartzProperties(quartzProperties);
+        factory.setAutoStartup(false);
+        return factory;
+    }
+
+    @Bean(name = "secondaryScheduler")
+    public SchedulerFactoryBean secondaryScheduler(JobFactory jobFactory, Properties quartzProperties) {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setJobFactory(jobFactory);
+        factory.setQuartzProperties(quartzProperties);
+        factory.setAutoStartup(false);
+        return factory;
+    }
+
+
+
+}
--- existing-quartz/src/main/java/it/fabioformosa/labs/existingquartz/controllers/LocalQuartzController.java
+++ existing-quartz/src/main/java/it/fabioformosa/labs/existingquartz/controllers/LocalQuartzController.java
@@ -1,12 +1,18 @@
 package it.fabioformosa.labs.existingquartz.controllers;
 
+import it.fabioformosa.quartzmanager.api.common.utils.Try;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.SchedulerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -14,13 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocalQuartzController {
 
     @Autowired
-    private Scheduler scheduler;
+    private List<Scheduler> schedulers;
 
     @GetMapping
-    public String getLocalQuartzSchedulerInstance() throws SchedulerException {
-        String schedulerName = scheduler.getSchedulerName();
-        log.info("Scheduler Instance Name is {}", schedulerName);
-        return schedulerName;
+    public List<String> getLocalQuartzSchedulerInstance() throws SchedulerException {
+        List<String> schedulerNames = schedulers.stream().map(Try.sneakyThrow(Scheduler::getSchedulerName)).toList();
+        log.info("Scheduler Instance Name is {}", schedulerNames);
+        return schedulerNames;
     }
 
 }
