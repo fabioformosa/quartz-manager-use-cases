53ebf170f2ad3aff1360f6dea0b74a938480f60d
fabio.formosa <fabio.formosa@gmail.com>
2022-10-22 10:24:25 +0200

    enabled the open-api-specification to the use case simply-spring

---
+++ simply-spring/src/main/java/it/fabioformosa/labs/simplyspring/controllers/SimpleController.java
@@ -0,0 +1,24 @@
+package it.fabioformosa.labs.simplyspring.controllers;
+
+import it.fabioformosa.quartzmanager.api.security.properties.InMemoryAccountProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/ping")
+public class SimpleController {
+
+    @Autowired
+    private InMemoryAccountProperties inMemoryAccountProperties;
+
+    @GetMapping
+    public String ping(){
+        log.debug("ping...");
+        return "pong";
+    }
+
+}
